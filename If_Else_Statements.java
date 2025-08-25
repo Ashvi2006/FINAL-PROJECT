1ac3f009cfb35e5c2cbe45c44d0cdc49cd8cb317
Ashvi2006 <24it001@charusat.edu.in>
2025-08-25 18:01:25 +0530

    Add files via upload

---
+++ If_Else_Statements.java
@@ -0,0 +1,13 @@
+public class If_Else_Statements {
+    public static void main(String[] args) {
+        int age=10;
+       
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+
+    }
+}
---
+++ data_types.java
@@ -0,0 +1,21 @@
+public class data_types {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int i = 1000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.99;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bool);
+    }
+}
